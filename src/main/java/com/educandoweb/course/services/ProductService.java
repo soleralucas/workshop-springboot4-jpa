016da765d4b926d33549e7a4eafcba1d37f6ac64
Lucas Pereira <lucassolera2006@gmail.com>
2026-02-24 23:03:15 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	
+	@Autowired //injeção de dependencia
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		
+		return repository.findAll();
+		
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();//retorna objeto do tipo Product que estiver dentro do Optional
+	}
+
+}
